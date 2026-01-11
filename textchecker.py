229bb4f6445f2c5859a012b0af5807b39b7bef54
ZushiBit <91222611+ZushiBit@users.noreply.github.com>
2026-01-10 22:41:59 -0500

    Main code made. I could add more advanced features as my skills progress.

--- textchecker.py
+++ textchecker.py
@@ -1,1 +1,8 @@
 
+text = input("Enter the original text. ")
+text2 = input("Enter the text you want to compare it to. ")
+
+if text == text2:
+    print("No differences found. ")
+elif text != text2
+    print("Differences found. ")
